12e97f4bc6feb2277e6b36a898fe38ebf3be6f86
Emanuel <guedesert@gmail.com>
2023-02-08 12:15:33 -0300

    Atualizado 08 de fev. de 2023 às 12:15

--- conversor/script.js
+++ conversor/script.js
@@ -1,5 +1,4 @@
 var formarea = document.querySelector("#area")
-var formau = document.querySelector(".alteraunidade")
 var bannerusuario = document.getElementById("usuario")
 var usuario =
   JSON.parse(localStorage.getItem("imersaodev@guedesert")) || "Viajante"
@@ -10,7 +9,6 @@ tela.addEventListener("change", ajustamenu)
 window.onload = carregadados
 
 formarea.addEventListener("input", calculaarea)
-formau.addEventListener("click", trocaunidade)
 
 function carregadados() {
   calculaarea()
@@ -29,80 +27,8 @@ function defineusuario() {
       usuario = "Viajante"
     }
   }
-
   localStorage.setItem("imersaodev@guedesert", JSON.stringify(usuario))
   bannerusuario.innerHTML = "Bem-vindo, " + usuario + "!"
-}
-function calculaarea() {
-  var viarea = document.getElementById("viarea").value
-  var uiarea = document.getElementById("uiarea").value
-  var ufarea = document.getElementById("ufarea").value
-  var vfarea = document.getElementById("vfarea")
-  if (viarea != "") {
-    viarea = parseFloat(viarea)
-    if (uiarea != "m2") {
-      if (uiarea == "mm2") {
-        var resultado = viarea / 1000
-      } else if (uiarea == "cm2") {
-        var resultado = viarea / 100
-      } else if (uiarea == "dm2") {
-        var resultado = viarea / 10
-      } else if (uiarea == "a") {
-        var resultado = viarea * 10
-      } else if (uiarea == "ha") {
-        var resultado = viarea * 100
-      } else if (uiarea == "km2") {
-        var resultado = viarea * 1000
-      }
-    } else {
-      var resultado = viarea
-    }
-    if (ufarea != "m2") {
-      if (ufarea == "mm2") {
-        resultado *= 1000
-      } else if (ufarea == "cm2") {
-        resultado *= 100
-      } else if (ufarea == "dm2") {
-        resultado *= 10
-      } else if (ufarea == "a") {
-        resultado /= 10
-      } else if (ufarea == "ha") {
-        resultado /= 100
-      } else if (ufarea == "km2") {
-        resultado /= 1000
-      }
-    }
-    vfarea.innerHTML = resultado
-  } else {
-    vfarea.innerHTML = ""
-  }
-}
-
-function trocaunidade() {
-  var uiarea = document.getElementById("uiarea").value
-  var ufarea = document.getElementById("ufarea").value
-  if (uiarea != ufarea) {
-    document.getElementById("uiarea").value = ufarea
-    document.getElementById("ufarea").value = uiarea
-    calculaarea()
-  }
-}
-
-function exibemenu() {
-  var div = document.getElementById("menuextendido").style.display
-  if (div == "none") {
-    document.getElementById("menuextendido").style.display = "flex"
-  } else {
-    document.getElementById("menuextendido").style.display = "none"
-  }
-}
-
-function ajustamenu(tela) {
-  if (tela.matches) {
-    document.getElementById("menuextendido").style.display = "none"
-  } else {
-    document.getElementById("menuextendido").style.display = "flex"
-  }
 }
 
 function definediv(div) {
@@ -188,3 +114,88 @@ function definediv(div) {
     sobre.display = "flex"
   }
 }
+
+function trocaunidade(grandeza) {
+  if (grandeza == 1) {
+    uiID = "uiarea"
+    ufID = "ufarea"
+  } else if (grandeza == 5) {
+    uiID = "uitemperatura"
+    ufID = "uftemperatura"
+  }
+  var ui = document.getElementById(uiID).value
+  var uf = document.getElementById(ufID).value
+  if (ui != uf) {
+    document.getElementById(uiID).value = uf
+    document.getElementById(ufID).value = ui
+    if (grandeza == 1) {
+      calculaarea()
+    } else if (grandeza == 5) {
+      calculatemperatura()
+    }
+  }
+}
+
+function calculaarea() {
+  var viarea = document.getElementById("viarea").value
+  var uiarea = document.getElementById("uiarea").value
+  var ufarea = document.getElementById("ufarea").value
+  var vfarea = document.getElementById("vfarea")
+  if (viarea != "") {
+    viarea = parseFloat(viarea)
+    if (uiarea != "m2") {
+      if (uiarea == "mm2") {
+        var resultado = viarea / 1000
+      } else if (uiarea == "cm2") {
+        var resultado = viarea / 100
+      } else if (uiarea == "dm2") {
+        var resultado = viarea / 10
+      } else if (uiarea == "a") {
+        var resultado = viarea * 10
+      } else if (uiarea == "ha") {
+        var resultado = viarea * 100
+      } else if (uiarea == "km2") {
+        var resultado = viarea * 1000
+      }
+    } else {
+      var resultado = viarea
+    }
+    if (ufarea != "m2") {
+      if (ufarea == "mm2") {
+        resultado *= 1000
+      } else if (ufarea == "cm2") {
+        resultado *= 100
+      } else if (ufarea == "dm2") {
+        resultado *= 10
+      } else if (ufarea == "a") {
+        resultado /= 10
+      } else if (ufarea == "ha") {
+        resultado /= 100
+      } else if (ufarea == "km2") {
+        resultado /= 1000
+      }
+    }
+    vfarea.innerHTML = resultado.toLocaleString("pt-BR")
+  } else {
+    vfarea.innerHTML = ""
+  }
+}
+
+function exibemenu() {
+  var div = document.getElementById("menuextendido").style.display
+  if (div == "none") {
+    document.getElementById("menuextendido").style.display = "flex"
+  } else {
+    document.getElementById("menuextendido").style.display = "none"
+  }
+}
+
+function ajustamenu(tela) {
+  if (tela.matches) {
+    document.getElementById("menuextendido").style.display = "none"
+  } else {
+    document.getElementById("menuextendido").style.display = "flex"
+  }
+}
+
+
